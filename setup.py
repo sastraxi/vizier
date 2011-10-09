5e87be007bcc1d50141a0890185f5ae4b61bff41
Cameron Gorrie <sastraxi@gmail.com>
2011-10-09 03:16:23 -0400

    Add cairoplot.handlers so setup.py install works

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='cairoplot-bsm',
     version='1.2dev',
-    packages=['cairoplot',],
+    packages=['cairoplot', 'cairoplot.handlers'],
     requires=['cairo',],
     license='GNU LGPL 2.1',
     url='https://github.com/sastraxi/cairoplot-bsm',
