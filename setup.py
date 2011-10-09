337e5add2d9fa9c867293bcd965b64a2b8c39a5b
Cameron Gorrie <sastraxi@gmail.com>
2011-10-09 02:48:44 -0400

    Add setup.py so this thang can be installed.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='cairoplot-bsm',
+    version='1.2dev',
+    packages=['cairoplot',],
+    license='GNU LGPL 2.1',
+    long_description=open('README.txt').read(),
+)
