8fe2aa70b9580f66973225a0812ee467eaa15b65
Cameron Gorrie <sastraxi@gmail.com>
2011-12-01 16:56:03 -0500

    Pretty-print threshold label values

--- vizier/series.py
+++ vizier/series.py
@@ -226,7 +226,9 @@ class Threshold(Series):
 
     def __init__(self, id, type, name, value):
         #Series.__init__(self, str(value) + " - " + type + ": " + name) # wait until colour change and bolding for "Warning" etc.
-        Series.__init__(self, '{0} - {1} [{2}]'.format(str(value), name, id))
+        def ppf(x):
+            return ('%f' % x).rstrip('0').rstrip('.')
+        Series.__init__(self, '{0} - {1} [{2}]'.format(ppf(value), name, id))
         self.type = type
         self.value = value
     
